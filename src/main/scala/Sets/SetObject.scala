8367089052abb8b43353841e21d89d3015912cb8
Abhishek Saxena <saxenaabhishek7944@gmail.com>
2023-04-27 11:43:13 +0530

    New changes added

---
+++ src/main/scala/Sets/SetObject.scala
@@ -0,0 +1,28 @@
+package Sets
+
+import java.util
+
+object SetObject extends App {
+  val s = Set(1,3,5,7,9)
+  println(s.tail)
+  println(s.isEmpty)
+  println(s"Minimum element is ${s.min}")
+  println(s"Maximum element is ${s.max}")
+  s.foreach(println)
+  for(x<- s) {
+    println(x)
+  }
+
+  val fruit1 = Set("apples", "oranges", "pears")
+  val fruit2 = Set("mangoes", "banana")
+  val fruit = fruit1 ++ fruit2
+  println(fruit)
+
+  val num1 = Set(5, 6, 9, 20, 30, 45)
+  val num2 = Set(50, 60, 9, 20, 35, 55)
+  println(s"Intersection of num1 and num2 is ${num1.intersect(num2)}")
+
+  //SorteSet
+  //val s: SortedSet[Int] = SortedSet(9,10,11,4,7)
+
+}
